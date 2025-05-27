85f7c1f2108f4e59b1840955ce952a3db4c20e51
Dominik Krenn <dominik.krenn@1tool.com>
2025-05-27 10:43:19 +0200

    Refactor MyLinkedList and Node classes for improved functionality and clarity

--- LinkedList/MyLinkedList.java
+++ LinkedList/MyLinkedList.java
@@ -1,53 +1,104 @@
 package LinkedList;
 
-/*
- * addFirst(String value)
- * addLast(String value)
- * addIndex(int index, String value)
- * deleteFirst()
- * deleteLast()
- * deleteIndex(int index)
- * printAll()
- */
-
 public class MyLinkedList<T> {
     private Node<T> head;
     private Node<T> tail;
-    // private Node<T> pointer;
+    private int size = 0;
 
+    public void addFirst(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
 
     public void addLast(T value) {
-        Node<T> new_node = new Node<T>(value);
+        Node<T> newNode = new Node<>(value);
+        if (tail == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
 
-        if (head == null) {
-            head = new_node;
-            head.next = new_node;
+    public void addIndex(int index, T value) {
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+        if (index == 0) {
+            addFirst(value);
             return;
         }
-
-
-        if (tail == null) {
-            tail = new_node;
+        if (index == size) {
+            addLast(value);
+            return;
         }
+        Node<T> curr = head;
+        for (int i = 0; i < index; i++) {
+            curr = curr.next;
+        }
+        Node<T> newNode = new Node<>(value);
+        newNode.prev = curr.prev;
+        newNode.next = curr;
+        curr.prev.next = newNode;
+        curr.prev = newNode;
+        size++;
+    }
+
+    public void deleteFirst() {
+        if (head == null) return;
+        if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+        size--;
+    }
+
+    public void deleteLast() {
+        if (tail == null) return;
+        if (head == tail) {
+            head = tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+        }
+        size--;
+    }
+
+    public void deleteIndex(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        if (index == 0) {
+            deleteFirst();
+            return;
+        }
+        if (index == size - 1) {
+            deleteLast();
+            return;
+        }
+        Node<T> curr = head;
+        for (int i = 0; i < index; i++) {
+            curr = curr.next;
+        }
+        curr.prev.next = curr.next;
+        curr.next.prev = curr.prev;
+        size--;
     }
 
     public void printAll() {
-        if (head == null && tail == null) {
-            System.out.println("Empty list");
+        Node<T> curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
         }
-
-        Node<T> ptr;
-
-        if (head != null) {
-            ptr = head;
-        } else {
-            ptr = tail;
-        }
-
-        while (ptr != null) {
-            System.out.println(ptr.value);
-            ptr = ptr.nextNode((head != null ? 1 : -1));
-        }
-        
+        System.out.println();
     }
 }
+
--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -1,27 +1,11 @@
 package LinkedList;
 
 public class Node<T> {
-    public Node<T> previous = null;
-    public Node<T> next = null;
-    public T value;
+    T data;
+    Node<T> prev;
+    Node<T> next;
 
-    public Node() {}
-
-    public Node(T value) {
-        this.value = value;
-    }
-
-    public Node(T value, Node<T> previous, Node<T> next) {
-        this.value = value;
-        this.previous = previous;
-        this.next = next;
-    }
-
-    public Node<T> nextNode(int direction) {
-        if (direction == 1) {
-            return this.next;
-        }
-
-        return this.previous;
+    Node(T data) {
+        this.data = data;
     }
 }
